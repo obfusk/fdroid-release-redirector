7af2bdaeaa009546b3b6ddbbbec47713b8cd6db8
FC Stegerman <flx@obfusk.net>
2023-01-03 05:11:28 +0100

    validate, support /uploads/, return 400/404

--- app.py
+++ app.py
@@ -2,27 +2,47 @@
 # SPDX-FileCopyrightText: 2023 FC Stegerman <flx@obfusk.net>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import re
+
 import requests
 
 from flask import Flask, abort, make_response, redirect
 
+# namespace, project, release
 GITLAB_RELEASE = "https://gitlab.com/api/v4/projects/{}%2F{}/releases/{}"
+# namespace, project, upload
+GITLAB_UPLOAD = "https://gitlab.com/{}/{}/uploads/{}"
 
 app = Flask(__name__)
 
 
+def validate(*args):
+    for arg in args:
+        if not re.fullmatch(r"[a-zA-Z0-9._-]+", arg):
+            raise ValueError(f"invalid: {arg!r}")
+
+
 @app.route("/gitlab/<namespace>/<project>/<release>/<asset>")
 def gitlab_release(namespace, project, release, asset):
+    try:
+        validate(namespace, project, release, asset)
+    except ValueError:
+        abort(400)
     url = GITLAB_RELEASE.format(namespace, project, release)
     try:
         req = requests.get(url, timeout=3)
         req.raise_for_status()
-        for asset_url in (a["url"] for a in req.json()["assets"]["links"]):
-            if asset_url.endswith(asset):
+        data = req.json()
+        for asset_url in (a["url"] for a in data["assets"]["links"]):
+            if asset_url.endswith("/" + asset):
                 return redirect(asset_url)
+        for upload in re.findall(r"\(/uploads/([0-9a-f]{32}/[^/)]+)\)", data["description"]):
+            if upload.endswith("/" + asset):
+                asset_url = GITLAB_UPLOAD.format(namespace, project, upload)
+                return redirect(asset_url)
+        abort(404)
     except (IndexError, KeyError, requests.RequestException):
-        pass
-    abort(500)
+        abort(400)
 
 
 @app.route("/robots.txt")
